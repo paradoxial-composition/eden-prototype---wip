cdd71c760d8fdb7d9cc80b3b2e5172154ff7815c
paradoxial-composition <elbani.othman@gmail.com>
2021-09-17 10:34:07 +0100

    Add item selection into cart

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Products, Navbar } from './components';
 
 const App = () => {
     const [products, setProducts] = useState([])
+    const [cart, setCart] = useState({})
 
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
@@ -11,15 +12,26 @@ const App = () => {
         setProducts(data);
     }
 
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve());
+    }
+
+    const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity);
+
+        setCart(item.cart);
+    }
     useEffect( () => {
         fetchProducts();
+        fetchCart();
     }, []);
 
-    console.log(products)
+    console.log(cart);
+    
     return (
         <div>
             <Navbar />            
-            <Products products={products} />
+            <Products products={products} onAddToCart={handleAddToCart}/>
         </div>
     )
 }
