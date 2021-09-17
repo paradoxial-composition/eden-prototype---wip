6152602c045cefe071f3b977a5f34922ea7d02e6
paradoxial-composition <elbani.othman@gmail.com>
2021-09-17 11:00:56 +0100

    Add Cart item component with style

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Container, Typography, Button, Grid } from '@material-ui/core'
+
+import useStyles from './styles';
+import CardItem from './CartItem/CartItem';
+
+const Cart = ({cart}) => {
+    const classes = useStyles();
+
+    const EmptyCart = () => (
+        <Typography variant="subtitle1">You have no items in your shopping cart, start adding some.</Typography>
+    );
+    const FilledCart = () => (
+        <>
+            <Grid container spacing={3}>
+                {cart.line_items.map((item) => (
+                    <Grid item xs={12} sm={4} key={item.id}>
+                        <CardItem item={item} />
+                    </Grid>
+                ))}
+            </Grid>
+            <div className={classes.cardDetails}>
+                <Typography variant="h4">Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
+                <div>
+                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">EmptyCard</Button>
+                    <Button className={classes.checkout} size="large" type="button" variant="contained" color="primary">Checkout</Button>
+                </div>
+            </div>
+        </>
+    );
+    
+    if(!cart.line_items) return 'Loading ...';
+
+    return (
+        <Container>
+            <div className={classes.toolbar} />
+            <Typography className={classes.title} variant="h3" gutterBottom>Your Shopping Cart</Typography>
+            { !cart.line_items.length ? <EmptyCart /> : <FilledCart />}
+        </Container>
+    ) 
+}
+
+export default Cart
