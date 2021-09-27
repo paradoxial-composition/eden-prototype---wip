2a87b4a16f1bd1f7271ebcf7f7f0f84b1d6837f4
paradoxial-composition <elbani.othman@gmail.com>
2021-09-27 12:33:10 +0100

    transaction finalisation handling via stripe

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -9,7 +9,7 @@ import PaymentForm from '../PaymentForm';
 
 const steps = ['Shipping adress', 'Payment details'];
 
-const Checkout = ({cart}) => {
+const Checkout = ({cart, order, onCaptureCheckout, error}) => {
     const classes = useStyles();
     const [activeStep, setActiveStep] = useState(0);
     const [checkoutToken, setCheckoutToken] = useState(null);
@@ -43,7 +43,7 @@ const Checkout = ({cart}) => {
 
     const Form = () => activeStep == 0
         ? <AddressForm checkoutToken={checkoutToken} next={next}/>
-        : <PaymentForm shippingData={setShippingData} checkoutToken={checkoutToken}/>
+        : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} backStep={backStep} onCaptureCheckout={onCaptureCheckout} nextStep={nextStep}/>
 
     return (
         <>
